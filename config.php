f915dd0970f5c3a37b32e33c4daad50e9fdf733f
sm4xas <sm4xas@hotmail.com>
2017-07-18 03:59:57 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// Configuration file for PHAM-PhpHomeAutomation
+
+// --- Database credentials ---
+$db_host = 	"db.myhost.com";
+$db_user = 	"user";
+$db_pass = 	"pass";
+$db_database = 	"database";
+$db_table_sensordata = 	"pham_sensordata";
+
+// --- Other credentials and settings
+
+// Password that sensors need to add data
+$sensorpwd = "sensorpass";
+
+?>
